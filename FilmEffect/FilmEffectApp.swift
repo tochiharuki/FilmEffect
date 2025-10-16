93b3834581c1f563192b9ced59fc8d76199d29aa
Haruki Tochishita <tochi.haruki@gmail.com>
2025-10-16 17:13:49 +0900

    Initial commit

---
+++ FilmEffect/FilmEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmEffectApp.swift
+//  FilmEffect
+//
+//  Created by Tochishita Haruki on 2025/10/16.
+//
+
+import SwiftUI
+
+@main
+struct FilmEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
