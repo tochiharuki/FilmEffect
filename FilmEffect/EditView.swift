90ba876e6d8e6f134846f2087fa80eb542cf853a
Haruki Tochishita <tochi.haruki@gmail.com>
2025-12-27 08:18:26 +0900

    “all”

--- FilmEffect/EditView.swift
+++ FilmEffect/EditView.swift
@@ -185,7 +185,7 @@ struct EditView: View {
             // 元画像
             image.draw(in: CGRect(origin: .zero, size: image.size))
     
-            let ratio: CGFloat = 1 / 15
+            let ratio: CGFloat = 1 / 20
     
             if whiteFrameType == .horizontal {
                 let h = image.size.height * ratio
